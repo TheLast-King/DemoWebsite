560c8b5c9105de6e5424900756ff83a9e9a2de43
MehulExplorer <rawal.mehul.sro@gmail.com>
2023-11-27 09:36:51 +0530

    First Commit

--- src/components/home/Footer/Footer.js
+++ src/components/home/Footer/Footer.js
@@ -31,11 +31,10 @@ const Footer = () => {
     <div className="w-full bg-[#F5F5F3] py-20">
       <div className="max-w-container mx-auto grid grid-cols-1 md:grid-cols-2  xl:grid-cols-6 px-4 gap-10">
         <div className="col-span-2">
-          <FooterListTitle title=" More about Orebi Shop" />
+          <FooterListTitle title=" More about Refurbicon Shop" />
           <div className="flex flex-col gap-6">
             <p className="text-base w-full xl:w-[80%]">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim sint
-              ab ullam, numquam nesciunt in.
+            Shop No. 38, OM Textiles Tower, Pur Road, Bhilwara, Rajasthan 311001 India.
             </p>
             <ul className="flex items-center gap-2">
               <a
